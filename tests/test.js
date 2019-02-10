68ff161a98021daf5ba12d15cb712c75de4a2cf3
Jiří Dušek <jironett@gmail.com>
2019-02-10 18:20:05 +0100

    Modified test.js

--- tests/test.js
+++ tests/test.js
@@ -1,22 +1,57 @@
 
-const config = require('./tests-config');
 const Configuration = require('@netcom/consilium-dei').Configuration;
-const https = require('https');
+const Thecla = require('../lib/Thecla');
+const logger = require('../lib/Logger');
+const http = require('http');
+const fs = require('fs');
+const config = require('./tests-config');
 
-const cfg = new Configuration(config.servers, thisServerId);
+/* ===== Create configuration schemes from config object ===== */
 
+const cfg1 = new Configuration(config.servers, 1);
+const cfg2 = new Configuration(config.servers, 2);
+
+/* ===== Create servers ===== */
+
+/*
 const tls =
     {
         cert: fs.readFileSync(config.cert),
         key: fs.readFileSync(config.key)
     };
-
+*/
 const emptyResult = (req, res) => {
     res.writeHead(404, 'Not Here');
     res.end();
 };
-const firstHttpsServer = https.createServer(tls, emptyResult);
-const secondHttpsServer = https.createServer(tls, emptyResult);
 
+const server1 = http.createServer(emptyResult);
+const server2 = http.createServer(emptyResult);
 
-const thecla = new Thecla(cfg, { server: theclaHttpsServer });
+/* ===== Create Thecla instances ===== */
+
+const thecla1 = new Thecla(cfg1, { server: server1 });
+const thecla2 = new Thecla(cfg2, { server: server2 });
+
+/* ===== Fire servers ===== */
+
+server1.listen(cfg1.getSelectedPoint().signalServerPort, cfg1.getSelectedPoint().hostname, () => {
+    logger.info('Thecla 1 is running.');
+
+    thecla1.fire((peer) => {
+        peer.on('error', (error) => {
+            logger.error(error);
+        });
+    });
+
+    server2.listen(cfg2.getSelectedPoint().signalServerPort, cfg2.getSelectedPoint().hostname, () => {
+        logger.info('Thecla 2 is running.');
+
+        thecla2.fire((peer) => {
+            peer.on('error', (error) => {
+                logger.error(error);
+            });
+        });
+    });
+
+});
